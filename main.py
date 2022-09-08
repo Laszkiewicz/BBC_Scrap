d326f87a3fb747aa70b0141e803c7ef78cfce90d
Laszkiewicz <laszkiewiczsebastiann@gmail.com>
2022-09-08 12:15:04 +0200

    BBC scrap 0.2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import re
 response = requests.get("https://www.bbc.com/news/world-60525350")
 
 bbc_web_page = response.text
@@ -16,8 +16,17 @@ for article_tag in articles:
     link = article_tag.get('href')
     article_links.append(link)
 
-article_time_posted = [time.getText() for time in soup.find_all(name="span", class_="gs-c-timestamp gs-o-bullet gs-o-bullet- nw-c-timestamp")]
-
+# article_time_posted = [time.getText().split('h')[0] for time in soup.find_all(name="span", class_="gs-c-timestamp gs-o-bullet gs-o-bullet- nw-c-timestamp")]
+article_time_posted = [time.getText().split('h')[0] for time in soup.find_all(name="span", class_="gs-c-timestamp gs-o-bullet gs-o-bullet- nw-c-timestamp")]
 print(article_texts)
 print(article_links)
-print(article_time_posted)
+
+
+article_time_posted = article_time_posted[2:-2]
+
+oldest_posted = max(article_time_posted)
+newest_posted = min(article_time_posted)
+new = article_time_posted.index(oldest_posted)
+new2 = article_time_posted.index(newest_posted)
+print(article_texts[new])
+print(article_texts[new2])
